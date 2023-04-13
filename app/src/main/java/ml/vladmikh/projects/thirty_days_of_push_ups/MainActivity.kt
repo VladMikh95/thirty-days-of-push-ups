0777e3eda025446b529e237c314d62e86d52403a
Vlad Mikhnenok <vlad.mikhnenok.95.@gmail.com>
2023-04-13 09:33:02 +0300

    First commit

---
+++ app/src/main/java/ml/vladmikh/projects/thirty_days_of_push_ups/MainActivity.kt
@@ -0,0 +1,11 @@
+package ml.vladmikh.projects.thirty_days_of_push_ups
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
